78f5f972f50039fe44ff366dd091fa1ea0e7b208
apf_temp_admin <apf_temp_admin@21-manic-012>
2023-09-20 13:36:06 +0900

    num1 변수값 변경

--- Jungol/src/q510/Main.java
+++ Jungol/src/q510/Main.java
@@ -1,12 +1,14 @@
 package q510;
 
 public class Main {
-
 	public static void main(String[] args) {
-		int a = -1;
-		int b = 100;
-		System.out.println(a);
-		System.out.println(b);
+		int num1 = -1;
+		int num2 = 100;
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		num1 = 99;
+		System.out.println(num1);
+		System.out.println(num2);
 	}
-
 }
