46987820b6cad5a3410b740a8e3a5360e4407d65
apf_temp_admin <apf_temp_admin@21-manic-012>
2023-09-21 17:43:07 +0900

    q534 풀이

--- Jungol/src/q534/Main.java
+++ Jungol/src/q534/Main.java
@@ -8,7 +8,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		char ar = sc.next().charAt(0);
-		
+		sc.close();
+		/*
 		switch(ar) {
 		case 'A' :
 			System.out.println("Excellent");
@@ -33,6 +34,21 @@ public class Main {
 		default :
 			System.out.println("error");
 			break;
+		}
+		*/
+		
+		if(ar == 'A') {
+			System.out.println("Excellent");
+		} else if(ar == 'B') {
+			System.out.println("Good");
+		} else if(ar == 'C') {
+			System.out.println("Usually");
+		} else if(ar == 'D') {
+			System.out.println("Effort");
+		} else if(ar == 'F') {
+			System.out.println("Failure");	
+		} else {
+			System.out.println("error");
 		}
 	}
 }
