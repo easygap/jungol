c66ecf758f3a208f95f28f1e97219546ee693dab
apf_temp_admin <apf_temp_admin@21-manic-012>
2023-09-22 17:43:12 +0900

    9월 22일 정올 문제풀이

--- Jungol/src/q506/Main.java
+++ Jungol/src/q506/Main.java
@@ -3,10 +3,15 @@ package q506;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("My height");
-		System.out.println("170");
-		System.out.println("My weight");
-		System.out.println("68.600000");
+		String a = "My height";
+		int b = 170;
+		String c = "My weight";
+		double d = 68.6;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.printf("%.6f",d);
 	}
 
 }
