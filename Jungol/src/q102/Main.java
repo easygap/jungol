e618be6268d1c534a9d97375aa4586001d1f638d
apf_temp_admin <apf_temp_admin@21-manic-012>
2023-09-22 09:32:13 +0900

    정올 문제풀이

---
+++ Jungol/src/q102/Main.java
@@ -0,0 +1,9 @@
+package q102;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("My hometown");
+		System.out.println("Flowering mountain");
+	}
+}
---
+++ Jungol/src/q504/Main.java
@@ -0,0 +1,12 @@
+package q504;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("(@) (@)");
+		System.out.println("(=^.^=)");
+		System.out.println("(-m-m-)");
+	}
+
+}
+
