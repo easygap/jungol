25ad3479882f4b76f80d41bb20b46015fd7cfc2a
apf_temp_admin <apf_temp_admin@21-manic-012>
2023-09-27 17:41:49 +0900

    2023 09 27 수업 예제 풀이

---
+++ JavaBook/src/java3/p30/Main.java
@@ -0,0 +1,24 @@
+package java3.p30;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Child child = new Child();
+		Parent parent = new Parent();
+		
+		parent.method1();
+		parent.method2();
+//			parent.method3();	// 호출 불가능
+		
+		child.method1();
+		child.method2();
+		child.method3();
+		
+		System.out.println("---------------------------");
+		
+		parent = child;
+		parent.method1();
+		parent.method2();
+//		parent.method3();	// 호출 불가능
+	}
+}
