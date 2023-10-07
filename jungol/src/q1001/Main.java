afdd4d90e7e7eb970338486b87f8772325d266cb
Administrator <Administrator@192.168.1.3>
2023-10-07 16:19:03 +0900

    2023 10 07 수정

---
+++ jungol/src/q1001/Main.java
@@ -0,0 +1,12 @@
+package q1001;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("번개장터\n찌릿찌릿뱅뱅뱅");
+	}
+}
