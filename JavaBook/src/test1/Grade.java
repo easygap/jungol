5f10c260ec924a0d5fb48172bfcec93068e3fad4
apf_temp_admin <apf_temp_admin@21-manic-012>
2023-10-18 17:41:32 +0900

    2023 10 18 예제 풀이

---
+++ JavaBook/src/test1/Grade.java
@@ -0,0 +1,18 @@
+package test1;
+
+public class Grade {
+	private int grade;
+
+	public void Grade() {
+
+	}
+
+	public void getGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public void setGrade() {
+		System.out.println("\tGrade : " + grade);
+	}
+	
+}
