b0295652b9d5f71c9fe900057a6f601a7bed573f
Administrator <Administrator@192.168.1.2>
2023-10-18 22:47:12 +0900

    2023 10 18 COMMIT!

---
+++ jungol/src/q521/Main.java
@@ -0,0 +1,22 @@
+package q521;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		sc.close();
+		
+		--num2;
+		int num3 = num1 * num2;
+		num1++;
+		
+		
+		System.out.printf("%d %d %d", num1, num2, num3);
+	}
+}
